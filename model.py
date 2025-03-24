Fixed the hyper parameters a bit, need to do more work to make sure hyper parameters of causal layer and NVAE and consistent

model.py
@@ -22,6 +22,7 @@ from thirdparty.inplaced_sync_batchnorm import SyncBatchNormSwish
 
 from concept_learning_template.conceptualizer import conceptualizer
 from typing import Optional
+from functools import reduce
 
 CHANNEL_MULT = 2
 
@@ -196,10 +197,11 @@ class AutoEncoder(nn.Module):
         self.num_power_iter = 4
 
     ### NEW CODE
+        self.decode_dim = reduce(lambda x, y: x * y, self.z0_size)
         if args.arch_flag != "vanilla":
             self.expressive_layer, self.causal_layer, self.unpool = conceptualizer(
                 args.eps_dim,
-                args.eps_in_width,
+                self.decode_dim // args.eps_dim,
                 args.eps_out_width,
                 args.eps_depth,
                 args.c_dim,
@@ -212,13 +214,18 @@ class AutoEncoder(nn.Module):
         self.arch_flag = args.arch_flag
     
     def conceptualize(self, z, batch_label: str):
+        shape = z.shape
         # our module
+        z = z.view(shape[0], -1)
         epsilon = self.expressive_layer(z)
         if self.arch_flag == "single-pooled-concept":
             c = self.causal_layer(epsilon)
         else:
             c = self.causal_layer[batch_label](epsilon)
-        return self.unpool(c)
+
+        c = self.unpool(c)
+
+        return c.view(shape)
     ### NEW CODE
 
     def init_stem(self):